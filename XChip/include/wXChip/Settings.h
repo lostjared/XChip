eaeccf6ed06c00a34bf1fde221b4ae4c3bcb5dde
lostjared <jaredbruni@gmail.com>
2016-04-14 12:50:22 -0700

    moved files around

---
+++ XChip/include/wXChip/Settings.h
@@ -0,0 +1,19 @@
+#ifndef __SETTINGS__H__
+#define __SETTINGS__H__
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+class SettingsWindow: public wxFrame {
+public:
+    SettingsWindow(const wxString &title, const wxPoint &pos, const wxSize);
+   
+private:
+     wxDECLARE_EVENT_TABLE();
+};
+
+
+
+#endif
