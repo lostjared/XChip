8481e693d1cb0c035a60fe5509d1b2150d9209c2
dhust <rafaelmoura.dev@gmail.com>
2016-04-18 14:34:31 -0300

    small revision on BaseTraits.h

--- XChip/include/XChip/Utility/BaseTraits.h
+++ XChip/include/XChip/Utility/BaseTraits.h
@@ -8,27 +8,46 @@ namespace xchip { namespace utility {
 struct true_type { static constexpr bool value = true; };
 struct false_type { static constexpr bool value = false; };
 
+// type_is: return the given type itself
+template<class T>
+struct type_is { using type = T; };
+
+
+
+
 // is same
 template<class T, class U>
 struct is_same : false_type {};
-
 template<class T>
 struct is_same<T, T> : true_type {};
 
+// C++14 variable tempalte
+//template<class T, class U>
+//constexpr bool is_same_v = is_same<T,U>::value;
 
 
 // is pointer
 template<class T>
 struct is_pointer : false_type {};
-
 template<class T>
 struct is_pointer<T*> : true_type {};
 
+// C++14 variable template
+//template<class T>
+//constexpr bool is_pointer_v = is_pointer<T>::value;
 
 
-
+// is reference
 template<class T>
-struct type_is { using type = T; };
+struct is_reference : false_type {};
+template<class T>
+struct is_reference<T&> : true_type {};
+template<class T>
+struct is_reference<T&&> : true_type {};
+
+// C++14 variable template
+//template<class T>
+//constexpr bool is_reference_v = is_reference<T>::value;
 
 
 // remove pointer
@@ -36,19 +55,20 @@ template<class T>
 struct remove_pointer : type_is<T> {} ;
 template<class T>
 struct remove_pointer<T*> : remove_pointer<T> {};
+
 template<class T>
 using remove_pointer_t = typename remove_pointer<T>::type;
 
 
 // remove cv
 template<class T>
-struct remove_cv : type_is<T> { };
+struct remove_cv : type_is<T> {};
 template<class T>
-struct remove_cv<volatile T> : type_is<T> { };
+struct remove_cv<volatile T> : type_is<T> {};
 template<class T>
-struct remove_cv<const T>  : type_is<T> { };
+struct remove_cv<const T>  : type_is<T> {};
 template<class T>
-struct remove_cv<const volatile T> : type_is<T> { };
+struct remove_cv<const volatile T> : type_is<T> {};
 
 template<class T>
 using remove_cv_t = typename remove_cv<T>::type;
@@ -56,21 +76,21 @@ using remove_cv_t = typename remove_cv<T>::type;
 
 //remove reference
 template<class T>
-struct remove_ref : type_is<T> { };
+struct remove_reference : type_is<T> {};
 template<class T>
-struct remove_ref<T&> : remove_ref<T> { };
+struct remove_reference<T&> : remove_reference<T> {};
 template<class T>
-struct remove_ref<T&&> : remove_ref<T> { };
+struct remove_reference<T&&> : remove_reference<T> {};
 
 template<class T>
-using remove_ref_t = typename remove_ref<T>::type;
+using remove_reference_t = typename remove_reference<T>::type;
 
 
 
 
 // remove all
 template<class T>
-struct remove_all : type_is< remove_cv_t < remove_pointer_t < remove_ref_t <T> > > > { };
+struct remove_all : type_is< remove_cv_t < remove_pointer_t < remove_reference_t <T> > > > {};
 
 
 template<class T>
