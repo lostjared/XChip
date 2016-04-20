8ac9bcf778651c777ed970363ac731394b328088
lostjared <jaredbruni@gmail.com>
2016-04-19 18:55:52 -0700

    updated SettingsWindow

--- XChip/src/wXChip/SettingsWindow.cpp
+++ XChip/src/wXChip/SettingsWindow.cpp
@@ -29,25 +29,16 @@ void SettingsWindow::setRomPath(const std::string &text)
 
 void SettingsWindow::CreateControls()
 {
-
 	using xchip::utility::make_unique;
-	
 	_panel = make_unique<wxPanel>(this, wxID_ANY);
 	_romText = make_unique<wxStaticText>(_panel.get(), ID_TEXT1,_T("Rom Path: "), wxPoint(10,15), wxSize(150,25));
 	_romPath = make_unique<wxTextCtrl>(_panel.get(), ID_TEXTCTRL1, "", wxPoint(100,10), wxSize(320,20),wxTE_READONLY);
-	
 	_fpsText = make_unique<wxStaticText>(_panel.get(), ID_TEXT2,_T("FPS: "), wxPoint(10,40), wxSize(150,25));
-
 	_fps = make_unique<wxTextCtrl>(_panel.get(), ID_TEXTCTRL1, _T("30"), wxPoint(100,40), wxSize(100,20));
-
 	_cpuText = make_unique<wxStaticText>(_panel.get(), ID_TEXT3,_T("CPU Freq: "), wxPoint(220,40), wxSize(150,25));
-	
 	_cpu = make_unique<wxTextCtrl>(_panel.get(), ID_TEXTCTRL3, _T(""), wxPoint(320,40), wxSize(100,20));
-	
 	_buttonOk = make_unique<wxButton>(_panel.get(), ID_BTN_OK, _T("Ok"), wxPoint(10, 150), wxSize(100,25));
 	_buttonCancel = make_unique<wxButton>(_panel.get(), ID_BTN_CANCEL, _T("Cancel"), wxPoint(120, 150), wxSize(100,25));
-
-	
 }
 
 void SettingsWindow::SaveSettings() {
