25c97287bfac7a5867fadce6042751bb7a5af8cb
lostjared <jaredbruni@gmail.com>
2016-04-25 10:17:59 -0700

    fixed insertitems

--- XChip/src/wXChip/MainWindow.cpp
+++ XChip/src/wXChip/MainWindow.cpp
@@ -110,7 +110,7 @@ void MainWindow::CreateControls()
 	wxArrayString strings;
 	_panel = make_unique<wxPanel>(this, wxID_ANY);
 	_text = make_unique<wxStaticText>(_panel.get(), ID_TEXT, _T("Chip8 Roms"), wxPoint(10,10), wxSize(100,25));
-	_listBox = make_unique<wxListBox>(_panel.get(), ID_LISTBOX, wxPoint(10, 35), wxSize(620, 360), strings, wxLB_SINGLE|wxLB_SORT);
+	_listBox = make_unique<wxListBox>(_panel.get(), ID_LISTBOX, wxPoint(10, 35), wxSize(620, 360), strings, wxLB_SINGLE);
 	_listBox->Connect(wxEVT_LEFT_DCLICK, wxMouseEventHandler(MainWindow::OnLDown), NULL, this);
 	_startRom = make_unique<wxButton>(_panel.get(), ID_STARTROM, _T("Start Rom"), wxPoint(10, 400), wxSize(100,25));
 	_settings = make_unique<wxButton>(_panel.get(), ID_LOADROM, _T("Load Roms"), wxPoint(120, 400), wxSize(100,25));
@@ -118,9 +118,6 @@ void MainWindow::CreateControls()
 	_settingsWin = make_unique<SettingsWindow>("wXChip - Settings", wxPoint(150, 150), wxSize(430, 220));
 	
 }
-
-
-
 
 void MainWindow::OnLDown(wxMouseEvent& event)
 {
