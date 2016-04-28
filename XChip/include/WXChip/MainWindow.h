dfb1c27868a0933b8483af024ff302c3b9567eb5
dhust <rafaelmoura.dev@gmail.com>
2016-04-27 22:42:37 -0300

    WXWINDOW RUNNING

---
+++ XChip/include/WXChip/MainWindow.h
@@ -0,0 +1,24 @@
+#ifndef _WXCHIP_MAINWINDOW_H_
+#define _WXCHIP_MAINWINDOW_H_
+
+
+#include <wx/frame.h>
+
+
+
+
+class MainWindow final : public wxFrame
+{
+public:
+	MainWindow(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+
+
+
+};
+
+
+
+
+
+#endif
---
+++ XChip/src/WXChip/Main.cpp
@@ -0,0 +1,47 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include <XChip/Utility/Memory.h>
+
+#include <WXChip/Main.h>
+#include <WXChip/MainWindow.h>
+
+
+
+
+wxIMPLEMENT_APP(WXChip);
+
+
+
+bool WXChip::OnInit()
+{
+	using xchip::utility::make_unique;
+
+	try {
+		auto frame = make_unique<MainWindow>("WXChip", wxPoint(50,50), wxSize(800, 600));
+		frame->Show(true);
+		frame.release();
+	}
+	catch(std::exception& err) {
+		std::cout << err.what() << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+
+
+int WXChip::OnExit()
+{
+	return 0;
+}
+
+
+
+
+
+
+
