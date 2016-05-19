3e65fbd487856c1bd004ed73eeba0083d1dffafa
dhust <rafaelmoura.dev@gmail.com>
2016-05-19 18:12:31 -0300

    GPLv3 license added

--- XChip/include/XChip/Core/Cpu.h
+++ XChip/include/XChip/Core/Cpu.h
@@ -1,3 +1,25 @@
+/*
+
+XChip - A chip8 lib and emulator.
+Copyright (C) 2016  Rafael Moura
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.html.
+
+*/
+
+
+
 #ifndef __XCHIP_CPU_H__
 #define __XCHIP_CPU_H__
 #include <XChip/Utility/StdintDef.h>
@@ -31,7 +53,12 @@ struct Cpu
 	uint8_t delayTimer;
 	uint8_t soundTimer;
 	
-	enum Flags : uint32_t { EXIT = 0x0000001, EXTENDED_MODE = 0x00000002 };
+	enum Flags : uint32_t 
+	{ 
+		EXIT = 0x0000001, 
+		EXTENDED_MODE = 0x00000002, 
+		PAUSE = 0x00000004 
+	};
 };
 
 
