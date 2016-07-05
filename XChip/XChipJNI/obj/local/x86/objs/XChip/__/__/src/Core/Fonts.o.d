1bf75d4f91582e3db3e2d87573d1b2e858069965
dhust <rafaelmoura.dev@gmail.com>
2016-07-05 11:31:40 -0300

    Cleanup

--- XChip/XChipJNI/obj/local/x86/objs/XChip/__/__/src/Core/Fonts.o.d
+++
@@ -1,8 +0,0 @@
-/home/dhust/Documents/Github/XChip/XChip/XChipJNI/obj/local/x86/objs/XChip/__/__/src/Core/Fonts.o: \
- /home/dhust/Documents/Github/XChip/XChip/XChipJNI/jni/../../src/Core/Fonts.cpp \
- ../../include/XChip/Core/Fonts.h \
- /home/dhust/Android/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint
-
-../../include/XChip/Core/Fonts.h:
-
-/home/dhust/Android/android-ndk-r11c/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint:
